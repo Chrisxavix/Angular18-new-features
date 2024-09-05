a6c655f5352f74da0ba3cc30ee64f7fe3f00029f
Chrisxavix <chrischillogallid64@outlook.com>
2024-09-04 22:51:39 -0500

    my-dashboard - create structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,57 @@
+import { Title } from '@angular/platform-browser';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "dashboard",
+    /* La clase dashboard se le pone dedault */
+    loadComponent: () => import("./dashboard/dashboard.component"),
+    children: [
+      {
+        path: "change-detection",
+        title: "Change Detection",
+        loadComponent: () => import("./dashboard/pages/change-detection/change-detection.component")
+      },
+      {
+        path: "control-flow",
+        title: "Control Flow",
+        loadComponent: () => import("./dashboard/pages/control-flow/control-flow.component")
+      },
+      {
+        path: "defer-option",
+        title: "Defer Option",
+        loadComponent: () => import("./dashboard/pages/defer-option/defer-option.component")
+      },
+      {
+        path: "defer-views",
+        title: "Defer Views",
+        loadComponent: () => import("./dashboard/pages/defer-views/defer-views.component")
+      },
+      {
+        path: "user/:id",
+        title: "User View",
+        loadComponent: () => import("./dashboard/pages/user/user.component")
+      },
+      {
+        path: "user-list",
+        title: "User List",
+        loadComponent: () => import("./dashboard/pages/user/user.component")
+      },
+      {
+        path: "view-transition",
+        title: "View Transition",
+        loadComponent: () => import("./dashboard/pages/view-transition/view-transition.component")
+      },
+      {
+        path: "",
+        redirectTo: "control-flow",
+        pathMatch: "full"
+      },
+    ]
+  },
+  {
+    path: "",
+    redirectTo: "/dashboard",
+    pathMatch: "full"
+  }
+];
