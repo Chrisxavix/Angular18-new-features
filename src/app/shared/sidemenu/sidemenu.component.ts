a9d19e577ea2da4656b70e8103d2259530a719d7
Chrisxavix <chrischillogallid64@outlook.com>
2024-09-09 22:43:30 -0500

    my-dashboard - sidebar

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
 export class SidemenuComponent {
 
+  public menuItems = routes
+  .map( route => route.children ?? [] )
+  .flat()
+  .filter( route => route && route.path)
+  .filter( route => !route.path?.includes(":"))
+
+
+  constructor() {
+    console.log(this.menuItems);
+
+  }
 }
